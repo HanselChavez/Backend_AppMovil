19c9c0e702e2a42e3962c14a2eac7cb4e1369653
Hansel Chavez <chavezhansel2901@gmail.com>
2025-05-31 20:40:55 -0500

    agregando url encoded

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import especialidadRoutes from "./routes/especialidadRoutes.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 dotenv.config();
 
 //app.use('/api/auth',authRoutes);
